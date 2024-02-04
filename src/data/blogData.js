7a3ad99789a5210731894bc45e83888abf50d368
Kalkidan Mesfin <mesfinkalkidan@gmail.com>
2024-02-03 22:31:41 -0500

    blog data removal

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -5,24 +5,6 @@ export const blogData = [
         description: 'What is Behavior-Driven Development (BDD)? How is it bridging the gap between stakeholders and developers.',
         date: 'May 23, 2023',
         image: 'https://miro.medium.com/v2/resize:fit:828/format:webp/1*nvg4Lm1lZtnf-PiVfIEpSQ.png',
-        url: 'https://medium.com/@beamlakalemseged/understanding-behavior-driven-development-bdd-bridging-the-gap-between-stakeholders-and-64b10058ae89'
+        url: ''
     },
 ]
-
-
-
-
-// Do not remove any fields.
-// Leave it blank instead as shown below.
-
-
-/* 
-{
-    id: 1,
-    title: 'Car Pooling System',
-    description: '',
-    date: 'Oct 1, 2020',
-    image: '',
-    url: 'https://preview.colorlib.com/theme/rezume/'
-}, 
-*/
